6475794be20edfae9a7351c6da232c5f3adaee7c
Aniket Pandharabale <aniketicloud@gmail.com>
2024-03-18 23:01:30 +0530

    final version for ssr react with vite

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 import express from "express";
 import { createRequestHandler } from "@remix-run/express";
 
-// notice that the result of `remix vite:build` is "just a module"
-import * as build from "./build/server/index.js";
+const viteDevServer =
+  process.env.NODE_ENV === "production"
+    ? null
+    : await import("vite").then((vite) => {
+        vite.createServer({
+          server: { middlewareMode: true },
+        });
+      });
 
 const app = express();
+app.use(
+  viteDevServer ? viteDevServer.middlewareMode : express.static("build/client")
+);
 
-app.use(express.static("build/cient"));
+
+const build = viteDevServer
+  ? () => viteDevServer.ssrLoadModule("virtual:remix/server-build")
+  : await import("./build/server/index.js");
 
 // all your app is "just a request handler"
 app.all("*", createRequestHandler({ build }));
